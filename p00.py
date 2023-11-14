80378b3cc01b867577f8c9e2e158fcc463aee815
Kuradusenge Martin <kuradusengemartin@gmail.com>
2023-11-14 13:19:51 +0200

    Two new py files added

---
+++ p00.py
@@ -0,0 +1,3 @@
+print("Welcome to Python programming. ")
+print("We gona use python for WS and API programming\n")
+print("Thank you for being interested with this course\n")
---
+++ p001.py
@@ -0,0 +1,6 @@
+num1 = float(input("Enter the first number\n"))
+num2 = float (input("Enter the second number\n"))
+print(num1,"+",num2,"=",num2+num2)
+print(num1,"-",num2,"=",num2-num2)
+print(num1,"/",num2,"=",num2/num2)
+print(num1,"*",num2,"=",num2*num2)
